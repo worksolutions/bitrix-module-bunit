64c4860a41ed1da4a49b3aeaeee7034af9216892
Максим Соколовский <sokolovsky@worksolutions.ru>
2017-04-21 17:57:28 +0300

    Редактирование команды помощи модуля

--- lib/command/helpcommand.php
+++ lib/command/helpcommand.php
@@ -13,19 +13,19 @@ class HelpCommand extends BaseCommand {
         $consoleWriter = $this->getConsole()->getWriter();
         $consoleWriter->nextLine();
         $consoleWriter->setColor(Output::COLOR_YELLOW)->printLine("Usage:");
-        $consoleWriter->setColor(0)->printLine("   php bunit.php <command> [<method>] [arg1] [arg2] ...");
+        $consoleWriter->setColor(0)->printLine("   php bunit <command> [<method>] [arg1] [arg2] ...");
         $consoleWriter->nextLine();
 
         $consoleWriter->setColor(Output::COLOR_YELLOW)->printChars("Help command:");
         $consoleWriter->setColor(0)->printLine(" Shows this manual for right usage");
         $consoleWriter->nextLine();
-        $consoleWriter->setColor(Output::COLOR_GREEN)->printLine("   php bunit.php help");
+        $consoleWriter->setColor(Output::COLOR_GREEN)->printLine("   php bunit help");
         $consoleWriter->nextLine();
 
         $consoleWriter->setColor(Output::COLOR_YELLOW)->printChars("Command run:");
         $consoleWriter->setColor(0)->printLine(" Runs testing process");
         $consoleWriter->nextLine();
-        $consoleWriter->setColor(Output::COLOR_GREEN)->printLine("   php bunit.php run [-<label>] [-<class>]");
+        $consoleWriter->setColor(Output::COLOR_GREEN)->printLine("   php bunit run [-<label>] [-<class>]");
         $consoleWriter->setColor(0)->printLine("     - [-<label>]       Includes tests which only have that label");
         $consoleWriter->setColor(0)->printLine("     - [-<class>]       Runs test only for pointed class");
         $consoleWriter->nextLine();
@@ -33,7 +33,7 @@ class HelpCommand extends BaseCommand {
         $consoleWriter->setColor(Output::COLOR_YELLOW)->printChars("Command db:");
         $consoleWriter->setColor(0)->printLine(" Provides several helpful functions");
         $consoleWriter->nextLine();
-        $consoleWriter->setColor(Output::COLOR_GREEN)->printLine("   php bunit.php db <method>");
+        $consoleWriter->setColor(Output::COLOR_GREEN)->printLine("   php bunit db <method>");
         $consoleWriter->setColor(0)->printLine("   Methods:");
         $consoleWriter->setColor(0)->printLine("     create       Creates mysql dump from original DB into system temporary file");
         $consoleWriter->setColor(0)->printLine("     update       Updates test DB from dump file");
