aa74bd42ad018098ae834d9b052d0daebfed47ae
Максим Соколовский <sokolovsky@worksolutions.ru>
2017-04-26 14:00:59 +0300

    Уствновщик composer принудительное конвертирование файлов

--- scripts/install.php
+++ scripts/install.php
@@ -9,5 +9,21 @@ define('CHK_EVENT', true);
 require($_SERVER["DOCUMENT_ROOT"]."/bitrix/modules/main/include/prolog_before.php");
 require(__DIR__ . '/../install/index.php');
 
+/** @var CMain $APPLICATION */
+global $APPLICATION;
+
 $module = new ws_bunit();
 $module->DoInstall();
+
+$di = new RecursiveDirectoryIterator(__DIR__ . '/../lang');
+
+/** @var SplFileInfo $fileInfo */
+foreach ($di as $fileInfo) {
+    if ($fileInfo->isDir()) {
+        continue;
+    }
+    $content = file_get_contents($fileInfo->getPath());
+    $convertedContent = $APPLICATION->ConvertCharset($content, LANG_CHARSET, "UTF-8");
+    file_put_contents($fileInfo->getPath(), $convertedContent);
+    $fileInfo->getPath();
+}
